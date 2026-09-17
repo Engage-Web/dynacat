c3cedd7bf83870aca0cb51395c7848a8d1fcad2d
Engage-Web-Tech-Team <tech@engageweb.co.uk>
2026-09-17 11:27:18 +0100

    Fix branch-triggered WordPress deployment

--- dynacat-plugin.php
+++ dynacat-plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: Dynamic filtering of Categories
  * Author: Engage Web - Steven Morris
- * Version: 1.32
+ * Version: 1.33
  * Author URI: https://www.engageweb.co.uk/about-us/meet-the-team#Steven
  * License: GPL2
  * Text Domain: dynacat
